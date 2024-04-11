275e87db5f2969992825e71fb1bcf5abdb949344
LstvJosel <jslpega@gmail.com>
2024-04-11 13:48:02 +0800

    Update

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+
 }
